cbfdebecfdd6f382689d293f87099db665781e13
AF-Nexus <afnexus01@gmail.com>
2025-10-27 21:11:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '😘', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💗'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/FHDEPkBBf281sUcdj17eU9?mode=wwt',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://i.postimg.cc/fbLksDqz/Screenshot-20251005-224142-Whats-App-Business.jpg',
+    NEWSLETTER_JID: '120363319098372999@newsletter',
+    NEWSLETTER_MESSAGE_ID: '5775',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: 'EF-PRIME HMD',
+    OWNER_NAME: 'Frankkaumbadev',
+    OWNER_NUMBER: '265993702468',
+    BOT_VERSION: '2.0.0 ʙᴇᴛᴀ',
+    BOT_FOOTER: '> Frankkaumbadev 😎',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029VbBMv2IDeON5eOz38p1M'
+};
