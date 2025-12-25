f68865292cb3dfc108d0e80992cdc07fecf7cd7d
AF-Nexus <afnexus01@gmail.com>
2025-12-25 16:16:48 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -2079,7 +2079,7 @@ function setupCommandHandlers(socket, number) {
                     }
 
                     try {
-                        const mentionedJid = msg.message.extendedTextMessage? . contextInfo?. mentionedJid || [];
+                        const mentionedJid = msg.message.extendedTextMessage?.contextInfo?. mentionedJid || [];
                         const user = mentionedJid[0];
 
                         if (!user) {
